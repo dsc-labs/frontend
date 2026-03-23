f3b0f397084219eab141fda716c63093b2b29f3b
expertdicer <expertdicer@gmail.com>
2026-03-23 11:21:26 +0700

    fix meta

--- src/components/Home/SocialIcons/SocialIcons.tsx
+++ src/components/Home/SocialIcons/SocialIcons.tsx
@@ -1,6 +1,9 @@
 import { motion } from 'framer-motion'
 import './SocialIcons.css'
 
+/** Update if your org/username differs */
+const GITHUB_URL = 'https://github.com/strikerobot'
+
 const SocialIcons = () => {
   return (
     <motion.div
@@ -14,6 +17,7 @@ const SocialIcons = () => {
         target="_blank"
         rel="noopener noreferrer"
         className="social-icon magnetic"
+        aria-label="X (Twitter)"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
       >
@@ -23,10 +27,29 @@ const SocialIcons = () => {
 
       </motion.a>
       <motion.a
+        href={GITHUB_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-icon magnetic"
+        aria-label="GitHub"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden>
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.021c0 4.428 2.865 8.184 6.839 9.504.5.092.682-.217.682-.483 0-.237-.009-.868-.013-1.703-2.782.605-3.369-1.341-3.369-1.341-.454-1.155-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482C19.138 20.203 22 16.447 22 12.021 22 6.484 17.522 2 12 2z"
+            fill="#1D1D1D"
+          />
+        </svg>
+      </motion.a>
+      <motion.a
         href="https://strikerobot.gitbook.io/strikerobot"
         target="_blank"
         rel="noopener noreferrer"
         className="social-icon magnetic"
+        aria-label="GitBook documentation"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
       >
