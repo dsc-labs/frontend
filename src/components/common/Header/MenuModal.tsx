8c325db6a10afb5ed21a775f8960c98f73418a21
expertdicer <expertdicer@gmail.com>
2026-05-04 23:05:19 +0700

    fix

--- src/components/common/Header/MenuModal.tsx
+++ src/components/common/Header/MenuModal.tsx
@@ -31,7 +31,6 @@ const MenuModal = ({ isOpen, onClose }: MenuModalProps) => {
       label: 'Publications',
     },
     { to: '/mindshare-challenge', label: 'Mindshare Challenge' },
-    { to: '/leaderboard', label: 'Leaderboard' },
   ]
 
   return (
